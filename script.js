7c984b623541414b3c27af46f0020644ac2cd66b
edison991 <86838293+edison991@users.noreply.github.com>
2022-04-14 20:31:14 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var convertKmToMiles = function (distanceInKm) {
+  var distanceInMiles = distanceInKm * 0.62;
+  return distanceInMiles;
+};
+
+var main = function (input) {
+  var myOutputValue = convertKmToMiles(input);
+  return myOutputValue;
+};
